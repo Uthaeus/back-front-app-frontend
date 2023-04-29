8743f5ced0e8b2ff54b5df9e87769cf02ec57eb2
Uthaeus <romanlavery@gmail.com>
2023-04-29 14:15:08 -0600

    added structure and styling to homepage

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,13 @@
-
+import { Link } from 'react-router-dom';
 
 function HomePage() {
 
     return (
-        <h1>home</h1>
+        <div className="home-container">
+            <h1 className="home-title">Welcome to Meetup App</h1>
+            <p className="home-description">This is a simple app to create and manage meetups</p>
+            <Link to='/meetups' className="home-link">Get Started</Link>
+        </div>
     );
 }
 
