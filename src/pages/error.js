cb51235e7c7edc1ab95c67378b09c85a37dd87be
Uthaeus <romanlavery@gmail.com>
2023-04-29 13:55:22 -0600

    added structure and styling to error element

--- src/pages/error.js
+++ src/pages/error.js
@@ -3,8 +3,9 @@ import { Link } from "react-router-dom";
 function ErrorPage() {
 
     return (
-        <div>
+        <div className="error-container">
             <h1>Error Page</h1>
+            <p>Sorry, the page you are looking for does not exist.</p>
             <Link to="/">Home</Link>
         </div>
     );
